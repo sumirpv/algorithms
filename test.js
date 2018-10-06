1719ed1709aa9ea04b488075911e2a90bd554c0a
sumirpv <sumirpv@gmail.com>
2018-10-06 08:45:57 -0700

    test

---
+++ test.js
@@ -0,0 +1,17 @@
+var ml= [1.2, 1.3, 1.4, 2.0, 2.1, 0.9];
+
+function find(fl, ml){
+  
+  for ( var i=0 ; i< ml.length ; i++){
+    for( var j=1; j< ml.length ; j++){
+     // console.log(" "+ ml[i]+" sdf " + ml[j]);
+      
+      if(ml[i]+ ml[j] === fl  ){
+        console.log("true"+ ml[i]+" sdf " + ml[j] + "fl"+ fl);
+      }
+    }
+  }
+ 
+}
+
+console.log(find(2.1 , ml))
