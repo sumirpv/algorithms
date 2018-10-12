33fa623d5aa6a8e730201d7b29bab158c7e75b2e
sumirpv <sumirpv@gmail.com>
2018-10-11 21:07:36 -0700

    updated files

--- test.js
+++ test.js
@@ -14,4 +14,60 @@ function find(fl, ml){
  
 }
 
+function find(fl, ml){
+  
+  for ( var i=0 ; i< ml.length ; i++){
+    for( var j=1; j< ml.length ; j++){
+     // console.log(" "+ ml[i]+" sdf " + ml[j]);
+      
+      if(ml[i]+ ml[j] === fl  ){
+        console.log("true"+ ml[i]+" sdf " + ml[j] + "fl"+ fl);
+      }
+    }
+  }
+ 
+}
+
+function find(fl, ml){
+  
+  for ( var i=0 ; i< ml.length ; i++){
+    for( var j=1; j< ml.length ; j++){
+     // console.log(" "+ ml[i]+" sdf " + ml[j]);
+      
+      if(ml[i]+ ml[j] === fl  ){
+        console.log("true"+ ml[i]+" sdf " + ml[j] + "fl"+ fl);
+      }
+    }
+  }
+ 
+}
+
+function find(fl, ml){
+  
+  for ( var i=0 ; i< ml.length ; i++){
+    for( var j=1; j< ml.length ; j++){
+     // console.log(" "+ ml[i]+" sdf " + ml[j]);
+      
+      if(ml[i]+ ml[j] === fl  ){
+        console.log("true"+ ml[i]+" sdf " + ml[j] + "fl"+ fl);
+      }
+    }
+  }
+ 
+}
+
+function find(fl, ml){
+  
+  for ( var i=0 ; i< ml.length ; i++){
+    for( var j=1; j< ml.length ; j++){
+     // console.log(" "+ ml[i]+" sdf " + ml[j]);
+      
+      if(ml[i]+ ml[j] === fl  ){
+        console.log("true"+ ml[i]+" sdf " + ml[j] + "fl"+ fl);
+      }
+    }
+  }
+ 
+}
+
 console.log(find(2.1 , ml))
